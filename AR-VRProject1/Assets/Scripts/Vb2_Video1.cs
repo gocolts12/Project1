a6de646ea0120c5202f486bf0c631c0d4feca8fd
mshawa2 <mshawa2@uic.edu>
2019-09-21 15:30:57 -0500

    working stuff 2

---
+++ AR-VRProject1/Assets/Scripts/Vb2_Video1.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Vb2_Video1 : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject vbB3bject;
+
+    public GameObject text1;
+   // public GameObject text2;
+    public GameObject Video1;
+    public GameObject Video2;
+    public int state;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbB3bject = GameObject.Find("Button3");
+        vbB3bject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+
+        Video1 = GameObject.Find("Vid1");
+        Video2 = GameObject.Find("Vid2");
+        text1 = GameObject.Find("Txt1");
+        //text2 = GameObject.Find("Txt2");
+
+        //if(Video1.active = false | Video2.active = false)
+        Video1.active = false;
+        Video2.active = false;
+        text1.active = true;
+        //text2.active = false;
+        state = 0;
+
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+
+        //here is where the button would display the character and play the recording
+        if (state == 0)
+        {
+            Video1.active = true;
+            Video2.active = false;
+            text1.active = false;
+        }
+        else if (state == 1)
+        {
+            Video1.active = false;
+            Video2.active = true;
+            text1.active = false;
+        }
+
+        //for debuging purposes
+        Debug.Log("BIN Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //here is where the button would make the character disapear and stop playing the recording
+
+        state++;
+       
+
+        //for debuging purposes
+        Debug.Log("BIN Realesed");
+    }
+
+    void Update()
+    {
+        if ( state == 2)
+        {
+            state = 0;
+            Video1.active = false;
+            Video2.active = false;
+            text1.active = true;
+        }
+
+    }
+}
---
+++ AR-VRProject1/Assets/Scripts/Vb2_Video2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Vb2_Video2 : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject vbB4bject;
+
+    public GameObject text1A;
+    public GameObject text2;
+    public GameObject Video1A;
+    public GameObject Video2A;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbB4bject = GameObject.Find("Button3");
+        vbB4bject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        Video1A = GameObject.Find("Vid1");
+        Video2A = GameObject.Find("Vid2");
+        text1A = GameObject.Find("Txt1");
+
+        Video1A.active = false;
+        Video2A.active = false;
+
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+
+        //here is where the button would display the character and play the recording
+        Video1A.active = false;
+        Video2A.active = true;
+        text1A.active = false;
+
+
+        //for debuging purposes
+        Debug.Log("BIN Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //here is where the button would make the character disapear and stop playing the recording
+        Video1A.active = false;
+        Video2A.active = false;
+        text1A.active = true;
+
+
+        //for debuging purposes
+        Debug.Log("BIN Realesed");
+    }
+}
+
